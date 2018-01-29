273af99b15cb36d20fc2d7b8b08a9c5d495b7ba0
MiguelNdeCarvalho <miguelndecarvalho@gmail.com>
2018-01-29 18:14:28 +0000

    AOSCP bringup

--- msm8956.mk
+++ msm8956.mk
@@ -123,8 +123,7 @@ PRODUCT_PACKAGES += \
     libqomx_core \
     libmm-qcamera \
     camera.device@1.0-impl \
-    android.hardware.camera.provider@2.4-impl \
-    SnapdragonCamera
+    android.hardware.camera.provider@2.4-impl 
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
@@ -154,10 +153,6 @@ PRODUCT_PACKAGES += \
     gralloc.msm8952 \
     hwcomposer.msm8952 \
     memtrack.msm8952
-
-# Doze mode
-PRODUCT_PACKAGES += \
-    XiaomiDoze
 
 # Fingerprint
 PRODUCT_PACKAGES += \

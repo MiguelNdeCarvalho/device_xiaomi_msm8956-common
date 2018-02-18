ca38b546c7ea617d997781b1c48c280401f78693
Adarsh-MR <adarshmr1998@gmail.com>
2018-02-18 05:39:10 +0000

    msm8956-common: build qc telephony and ims extensions
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- msm8956.mk
+++ msm8956.mk
@@ -315,6 +315,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9335.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_wcd9335.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext \
+    ims-ext-common
+
+PRODUCT_BOOTJARS += \
+    telephony-ext
+
 # WCNSS
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
